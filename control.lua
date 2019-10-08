99a40c13b71c494a022cd6b5164a18fc9a120506
Tawaxaan <tawaxaan@gmail.com>
2019-10-08 23:10:03 +0500

    Change daytime adjust method

--- control.lua
+++ control.lua
@@ -1,25 +1,12 @@
-local config = require( 'config' )
+local settings = require( 'settings' )
 
 -------------------------------------------------------------------------------- Daytime ajust
 
-global.checks_count = 0
+-- Length of day (in real minutes).
+local day_length = 20
 
--- Run check every N tick.
-local n_tick = 40
-
--- Unfreeze daytime every N check.
-local unfreeze_period = 3
-
-script.on_nth_tick( n_tick, function( ev )
-     if global.checks_count < 2 then
-         for _, s in pairs( game.surfaces ) do
-             s.freeze_daytime = ( global.checks_count == 1 )
-         end
-     end
-     global.checks_count = global.checks_count + 1
-     if global.checks_count > unfreeze_period then
-         global.checks_count = 0
-     end
+script.on_init( function()
+    game.surfaces[ 1 ].ticks_per_day = day_length * 60 * 60
 end)
 
 --------------------------------------------------------------------------------
