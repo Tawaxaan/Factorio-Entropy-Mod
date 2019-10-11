9752417774f3846640cd9c43e0157c2d3c727a11
Tawaxaan <tawaxaan@gmail.com>
2019-10-11 19:30:22 +0500

    Change daytime adjust value

--- control.lua
+++ control.lua
@@ -3,7 +3,7 @@ local settings = require( 'settings' )
 -------------------------------------------------------------------------------- Daytime ajust
 
 -- Length of day (in real minutes).
-local day_length = 20
+local day_length = 24
 
 script.on_init( function()
     game.surfaces[ 1 ].ticks_per_day = day_length * 60 * 60
