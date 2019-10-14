e55f0b0c044ef7d3aede00e0d23abd7b12d43f11
Tawaxaan <tawaxaan@gmail.com>
2019-10-15 01:19:29 +0500

    + Alien Biomes mod
    Dependency and tuning.

--- data.lua
+++ data.lua
@@ -1,3 +1,15 @@
 require( "prototypes.hotkeys" )
 require( "code.entities"      )
 require( "code.terrain"       )
+
+-------------------------------------------------------------------------------- Alien Biomes configure
+-- Forced applying Alien Biomes alien-biomes-remove-obsolete-tiles startup setiing.
+settings.startup[ "alien-biomes-remove-obsolete-tiles" ].value = true
+
+-- Alien Biomes does not remove this tile.
+data.raw.tile[ "grass-1" ] = nil
+
+-- Vegetation always enabled.
+settings.startup["alien-biomes-disable-vegetation"].value = false
+
+--------------------------------------------------------------------------------
