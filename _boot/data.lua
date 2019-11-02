471b790ac691f17f28e690fa11c53360d08c38f3
Tawaxaan <tawaxaan@gmail.com>
2019-11-02 20:11:05 +0500

    + Recipes and technologies wipe

--- _boot/data.lua
+++ _boot/data.lua
@@ -1,20 +1,22 @@
 -------------------------------------------------------------------------require
-local util   = require( 'util'  )
+local util  = require( 'util'  )
 --------------------------------------------------------------------------config
 ---------------------------------------------------------------------------local
 
+--______________________________________________________________________________________________________________________
+--############################################################################## MODS SETTINGS OVERRIDES ###############
 
--------------------------------------------------------------------------------- Alien Biomes configure
--- Forced applying Alien Biomes alien-biomes-remove-obsolete-tiles startup setiing.
+-------------------------------------------------------------------------------- Alien Biomes
+-- Forced applying alien-biomes-remove-obsolete-tiles startup setiing
 settings.startup[ 'alien-biomes-remove-obsolete-tiles' ].value = true
 
--- Alien Biomes does not remove this tile.
+-- Alien Biomes does not remove this tile
 data.raw.tile[ 'grass-1' ] = nil
 
--- Blocking tile-speed-reduction setting.
+-- Blocking tile-speed-reduction setting
 settings.startup[ 'tile-speed-reduction' ].value = 100
 
--- Vegetation always enabled.
+-- Vegetation always enabled
 settings.startup[ 'alien-biomes-disable-vegetation' ].value = false
 
 -- Includes
@@ -61,3 +63,72 @@ settings.startup[ 'alien-biomes-include-inland-shallows'  ].value = true
 settings.startup[ 'alien-biomes-include-coastal-shallows' ].value = true
 
 --------------------------------------------------------------------------------
+
+--______________________________________________________________________________________________________________________
+--############################################################################## WIPE THIS WORLD ### HO-HO-HO ##########
+
+local ignoredRecipies = {
+    'rail', 'locomotive', 'cargo-wagon', 'train-stop', 'rail-signal', 'rail-chain-signal',
+    'roboport', 'logistic-chest-passive-provider', 'logistic-chest-storage', 'construction-robot',
+    'exoskeleton-equipment', 'personal-roboport-equipment',
+    'storage-tank', 'pump', 'empty-barrel',
+    'pumpjack', 'oil-refinery', 'chemical-plant', 'basic-oil-processing', 'solid-fuel-from-petroleum-gas',
+    'advanced-oil-processing', 'sulfuric-acid', 'coal-liquefaction', 'heavy-oil-cracking', 'light-oil-cracking',
+    'solid-fuel-from-light-oil', 'solid-fuel-from-heavy-oil', 'lubricant',
+    'iron-plate', 'copper-plate', 'steel-plate', 'stone-brick', 'sulfur', 'plastic-bar',
+    'copper-cable', 'iron-stick', 'iron-gear-wheel', 'electronic-circuit', 'advanced-circuit',
+    'processing-unit', 'engine-unit', 'electric-engine-unit',
+    'uranium-processing', 'uranium-fuel-cell', 'kovarex-enrichment-process',
+    'explosives', 'battery', 'flying-robot-frame', 'low-density-structure',
+    'rocket-part', 'rocket-fuel', 'rocket-control-unit',
+    'automation-science-pack', 'logistic-science-pack', 'chemical-science-pack', 'military-science-pack',
+    'production-science-pack', 'utility-science-pack',
+    --[[ Exception for Time-Tool mod ]] 'red-wire', 'green-wire', 'arithmetic-combinator', 'decider-combinator',
+    --[[ Exception for Time-Tool mod ]] 'constant-combinator', 'power-switch', 'programmable-speaker'
+}
+
+local ignoredTechnologies = {
+    'construction-robotics', 'personal-roboport-equipment', 'exoskeleton-equipment', 'character-logistic-slots-1',
+    'fluid-handling', 'oil-processing', 'railway', 'automated-rail-transportation', 'rail-signals',
+    --[[ Exception for Time-Tool mod ]] 'circuit-network'
+}
+
+-------------------------------------------------------------------------------- Turn-off and remove all recipies
+for k, v_recp in pairs( data.raw.recipe ) do
+    if v_recp.normal then
+        v_recp.normal.enabled    = false
+        v_recp.expensive.enabled = false
+    else
+        v_recp.enabled =  false
+    end
+end
+
+local skipList = {}
+for _, v in pairs( ignoredRecipies ) do skipList[ v ] = true end
+
+for k, v_recp in pairs( data.raw.recipe ) do
+    if not skipList[ v_recp.name ] then
+        data.raw.recipe[ k ] = nil
+    end
+end
+
+-------------------------------------------------------------------------------- Turn-off and remove all technologies
+for k, v_tech in pairs( data.raw.technology ) do
+    v_tech.enabled       = false
+    v_tech.hidden        = true
+    v_tech.prerequisites = nil
+end
+
+local skipList = {}
+for _, v in pairs( ignoredTechnologies ) do skipList[ v ] = true end
+
+for k, v_tech in pairs( data.raw.technology ) do
+    if not skipList[ v_tech.name ] then
+        data.raw.technology [ k ] = nil
+    end
+end
+
+--------------------------------------------------------------------------------
+
+--______________________________________________________________________________________________________________________
+--######################################################################################################################
