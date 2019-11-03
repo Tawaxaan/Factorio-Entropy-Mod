511b532b6481766bcab02fe0a06ad5e78df453e1
Tawaxaan <tawaxaan@gmail.com>
2019-11-03 13:38:30 +0500

    Scaled tree minable and stump adjust

--- #terrain/forest_cfg.lua
+++ #terrain/forest_cfg.lua
@@ -5,14 +5,15 @@ config.CHUNKS_COUNT_UPDATE_INTERVAL = 5
 -- Interval for processing iteration (tick)
 config.PROCESS_INTERVAL             = 1
 
--- Count of tree growth
+-- Count of tree growth (must be a multiple of four)
 config.TREE_GROWTH_STAGES           = 16
 
 -- Suffix for scaled prototypes names
 config.TREE_SCALE_SUFFIX   = 'scl'
 
 -- Group and subgroups for rescaled trees
-config.TREE_SCALE_GROUP    = 'trees-scaled'
-config.TREE_SCALE_SUBGROUP = 'trees-scaled-normal'
+config.TREE_SCALE_GROUP          = 'tree-scaled'
+config.TREE_SCALE_SUBGROUP_TREE  = 'tree-scaled-normal'
+config.TREE_SCALE_SUBGROUP_STUMP = 'tree-scaled-stump'
 
 return config
--- #terrain/forest_dat.lua
+++ #terrain/forest_dat.lua
@@ -4,12 +4,69 @@ local _gfx  = require( 'common/gfx'  )
 --------------------------------------------------------------------------config
 config = require( '#terrain/forest_cfg' )
 ---------------------------------------------------------------------------local
-local TREE_GROWTH_STAGES  = config.TREE_GROWTH_STAGES
-local TREE_SCALE_SUFFIX   = config.TREE_SCALE_SUFFIX
-local TREE_SCALE_GROUP    = config.TREE_SCALE_GROUP
-local TREE_SCALE_SUBGROUP = config.TREE_SCALE_SUBGROUP
 
-local TINY_PARTICLE_SUFFIX = '-tiny'
+
+--______________________________________________________________________________________________________________________
+--############################################################################## TREES TINT CORRECTION #################
+
+local tintColors = {}
+
+-- Volcanic
+tintColors[ 'tree-volcanic-a' ] = {}
+tintColors[ 'tree-volcanic-a' ].leaves = { r = 200, g = 200, b = 255 }
+tintColors[ 'tree-volcanic-a' ].trunk  = { r = 100, g = 120, b = 120 }
+
+-------------------------------------------------------------------------------- Applying tint
+for treeName, treesColor in pairs( tintColors ) do
+    local tree = data.raw.tree[ treeName ]
+    for _, v_var in pairs ( tree.variations ) do
+        local leavesColor = tintColors[ treeName ].leaves
+        v_var.leaves.tint = leavesColor
+        if v_var.leaves.hr_version then
+            v_var.leaves.hr_version.tint = leavesColor
+        end
+        local trunkColor = tintColors[ treeName ].trunk
+        v_var.trunk.tint = trunkColor
+        if v_var.trunk.hr_version then
+           v_var.trunk.hr_version.tint = trunkColor
+        end
+    end
+end
+
+--------------------------------------------------------------------------------
+
+--______________________________________________________________________________________________________________________
+--############################################################################## SCALED TREES GENERATION ###############
+
+local TREE_GROWTH_STAGES         = config.TREE_GROWTH_STAGES
+local TREE_SCALE_SUFFIX          = config.TREE_SCALE_SUFFIX
+local TREE_SCALE_GROUP           = config.TREE_SCALE_GROUP
+local TREE_SCALE_SUBGROUP_TREE   = config.TREE_SCALE_SUBGROUP_TREE
+local TREE_SCALE_SUBGROUP_STUMP  = config.TREE_SCALE_SUBGROUP_STUMP
+
+local TINY_PARTICLE_SUFFIX       = '-tiny'
+
+data:extend({
+    {
+        type  = "item-group",
+        name  = TREE_SCALE_GROUP,
+        order = "e",
+        icon  = "__entropy__/graphics/icons/groups/scaled-trees.png",
+        icon_size = 64,
+    },
+    {
+        type  = "item-subgroup",
+        name  = TREE_SCALE_SUBGROUP_TREE,
+        group = TREE_SCALE_GROUP,
+        order = "a",
+    },
+    {
+        type  = "item-subgroup",
+        name  = TREE_SCALE_SUBGROUP_STUMP,
+        group = TREE_SCALE_GROUP,
+        order = "b",
+    },
+})
 
 -------------------------------------------------------------------------------- Create small particles for small trees
 local  function scaledAnimationVariations( oldAnimationVariations, scale )
@@ -28,125 +85,135 @@ local function scaledParticle( oldParticle, suffix, scale )
     return newParticle
 end
 
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 data:extend({
-    scaledParticle( data.raw[ 'leaf-particle' ][ 'leaf-particle'   ], TINY_PARTICLE_SUFFIX, 0.01 ),
-    scaledParticle( data.raw[ 'particle'      ][ 'branch-particle' ], TINY_PARTICLE_SUFFIX, 0.01 ),
+    scaledParticle( data.raw[ 'leaf-particle' ][ 'leaf-particle'   ], TINY_PARTICLE_SUFFIX, 0.2 ),
+    scaledParticle( data.raw[ 'particle'      ][ 'branch-particle' ], TINY_PARTICLE_SUFFIX, 0.2 ),
+    scaledParticle( data.raw[ 'particle'      ][ 'wooden-particle' ], TINY_PARTICLE_SUFFIX, 0.5 ),
 })
 
--------------------------------------------------------------------------------- TEMP
----- DEV RECOLOR --------------
----- DEV RECOLOR --------------
----- DEV RECOLOR --------------
+-------------------------------------------------------------------------------- Create small stumps for small trees
+for _, v_corpse in pairs( data.raw.corpse ) do
+    local skip = not util.string_starts_with( v_corpse.name, 'tree' )
+                 or v_corpse.group == TREE_SCALE_GROUP
+    if not skip then
+        for i_size = 1, TREE_GROWTH_STAGES / 4 do
+            local newStump = table.deepcopy( v_corpse )
+            newStump.name  = v_corpse.name .. '-' .. TREE_SCALE_SUFFIX .. i_size
+            newStump.group = TREE_SCALE_GROUP
+            newStump.subgroup = TREE_SCALE_SUBGROUP_STUMP
+            newStump.autoplace = nil
+            local scale = 0.1 + (i_size - 1) * 0.80 / ( TREE_GROWTH_STAGES / 4 )
 
-    local tree = data.raw.tree['tree-volcanic-a']
-    for k, v in pairs ( tree.variations ) do
-        local color = { r = 200, g = 200, b = 255 }
-        v.leaves.tint = color
-        if v.leaves.hr_version then
-            v.leaves.hr_version.tint = color
-        end
-        local color = { r = 120, g = 160, b = 200 }
-        v.trunk.tint = color
-        if v.trunk.hr_version then
-           v.trunk.hr_version.tint = color
+            local oldAnims = newStump.animation
+            local newAnims = {}
+
+            for k, v_oldAnim in pairs( oldAnims ) do
+                local newAnim = _gfx.sprite_scale( v_oldAnim, scale )
+                newStump.animation[ k ] = newAnim
+            end
+
+            --for _, v_anim in pairs( newStump.animation ) do
+                --v_anim = _gfx.sprite_scale( v_anim, scale )
+            --end
+            data:extend({ newStump })
         end
     end
----- DEV RECOLOR --------------
----- DEV RECOLOR --------------
----- DEV RECOLOR --------------
-data:extend({
-  {
-    type = "item-group",
-    name = TREE_SCALE_GROUP,
-    order = "e",
-    inventory_order = "e",
-    icon = "__entropy__/graphics/icons/groups/scaled-trees.png",
-    icon_size = 64,
-  },
-  {
-    type = "item-subgroup",
-    name = TREE_SCALE_SUBGROUP,
-    group = TREE_SCALE_GROUP,
-    order = "a",
-  },
-})
+end
+
 -------------------------------------------------------------------------------- Create small trees for all trees
-for _, tree in pairs( data.raw.tree ) do
-    if tree.group ~= TREE_SCALE_GROUP then
-    --if oldTree.name:find("dead") or oldTree.name:find("dry") then
-        for i_stage = 1, TREE_GROWTH_STAGES do
-            -- Naming and options
-            local newTree = table.deepcopy( tree )
-            newTree.name = tree.name .. '-' .. TREE_SCALE_SUFFIX
-            if i_stage < 10 then
-                newTree.name = newTree.name .. '0' .. i_stage
-            else
-                newTree.name = newTree.name .. i_stage
-            end
-            newTree.group = TREE_SCALE_GROUP
-            newTree.subgroup = TREE_SCALE_SUBGROUP
-            newTree.autoplace = nil
+function CreateSmallTree( parrentTree, stage )
+    local newTree = table.deepcopy( parrentTree )
 
-    --! tree.max_health = round(tree.max_health * areaScale)
-    --tree.collision_box = scaledBox(tree.collision_box, sqrt(areaScale))
-    --tree.selection_box = scaledBox(tree.selection_box, sqrt(areaScale))
+    -- Naming and options
+    local suffix  = TREE_SCALE_SUFFIX .. ( stage < 10 and 0 or '' ) .. stage
+    newTree.name  = parrentTree.name .. '-' .. suffix
+    newTree.group = TREE_SCALE_GROUP
+    newTree.subgroup = TREE_SCALE_SUBGROUP_TREE
+    newTree.autoplace = nil
 
-            -- Calc scale for small tree
-            local scale = 0.15 + (i_stage - 1) * 0.80 / TREE_GROWTH_STAGES
+    -- Calc scale for small tree
+    local scale = 0.15 + (stage - 1) * 0.80 / TREE_GROWTH_STAGES
 
-            -- Resizing drawing box
-            if newTree.drawing_box then
-                newTree.drawing_box = _math.box_scale( newTree.drawing_box, scale )
-            end
+    -- Max health
+    newTree.max_health = parrentTree.max_health * scale
 
-            -- Resizing sprites
-            if newTree.pictures then
-                local oldPictures = newTree.pictures
-                local newPictures = {}
-                for i, oldSprite in ipairs( oldPictures ) do
-                    newPictures[ i ] = _gfx.sprite_scale( oldSprite, scale )
-                end
-                newTree.pictures = newPictures
-            end
+    -- Mining
+    newTree.minable.count = parrentTree.minable.count * scale
+    if scale <= 0.6 then
+        newTree.minable.mining_particle = "wooden-particle" .. TINY_PARTICLE_SUFFIX
+    end
 
-            -- Resize or create particles sprites
-            if newTree.variations then
-                local newVariations = {}
-                for i, oldVariation in ipairs( newTree.variations ) do
-                    local newVariation = {}
-                    for j, oldPic in pairs( oldVariation ) do
-                        if not oldPic.type then
-                            newVariation[ j ] = _gfx.sprite_scale( oldPic, scale )
-                        elseif oldPic.type == 'create-particle' then
-                            ---- CPTEI = CreateParticleTriggerEffectItem
-                            local oldCptei = oldPic
-                            local newCptei = table.deepcopy( oldCptei )
-                            if scale <= 0.4 then
-                                newCptei.entity_name = newCptei.entity_name .. TINY_PARTICLE_SUFFIX
-                            end
-                            local p = { 'initial_height'          ,
-                                        'initial_height_deviation',
-                                        'initial_vertical_speed'  ,
-                                        'speed_from_center'       ,
-                                        'speed_from_center_deviation' }
-                            for k, v in pairs( p ) do
-                                if oldCptei[ k ] then
-                                    newCptei[ k ] = v * scale
-                                end
-                            end
-                            newCptei.offset_deviation = _math.box_scale( oldCptei.offset_deviation, scale )
-                            newVariation[ j ] = newCptei
-                        end
+    -- Sorpse (stump)
+    if newTree.remains_when_mined then
+        local suffix  = TREE_SCALE_SUFFIX .. math.floor( ( stage + 3 ) / 4 )
+        newTree.remains_when_mined = parrentTree.remains_when_mined .. '-' .. suffix
+    end
+
+    -- Loot
+
+    -- if parrentTree.loot then
+    --     for k, v_loot in pairs( parrentTree.loot ) do
+    --         v_loot.count_min   = 0
+    --         v_loot.count_max   = parrentTree.loot[ k ].count_max   * scale
+    --         v_loot.probability = parrentTree.loot[ k ].probability * scale
+    --     end
+    -- end
+
+    -- Resizing drawing box
+    if newTree.drawing_box then
+        newTree.drawing_box = _math.box_scale( newTree.drawing_box, scale )
+    end
+
+    -- Resizing sprites
+    if newTree.pictures then
+        local oldPictures = newTree.pictures
+        local newPictures = {}
+        for i, oldSprite in ipairs( oldPictures ) do
+            newPictures[ i ] = _gfx.sprite_scale( oldSprite, scale )
+        end
+        newTree.pictures = newPictures
+    end
+
+    -- Resize or create particles sprites
+    if newTree.variations then
+        local newVariations = {}
+        for i, oldVariation in ipairs( newTree.variations ) do
+            local newVariation = {}
+            for j, oldPic in pairs( oldVariation ) do
+                if not oldPic.type then
+                    newVariation[ j ] = _gfx.sprite_scale( oldPic, scale )
+                elseif oldPic.type == 'create-particle' then
+                    ---- CPTEI = CreateParticleTriggerEffectItem
+                    local oldCptei = oldPic
+                    local newCptei = table.deepcopy( oldCptei )
+                    if scale <= 0.6 then
+                        newCptei.entity_name = newCptei.entity_name .. TINY_PARTICLE_SUFFIX
                     end
-                    newVariations[i] = newVariation
+                    newCptei[ 'initial_height' ] = newCptei[ 'initial_height' ] * scale
+                    newCptei.offset_deviation    = _math.box_scale( oldCptei.offset_deviation, scale )
+                    newVariation[ j ] = newCptei
                 end
-                newTree.variations = newVariations
             end
+            newVariations[i] = newVariation
+        end
+        newTree.variations = newVariations
+    end
 
-            --
-            data:extend({ newTree })
+    --
+    data:extend({ newTree })
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+for _, v_tree in pairs( data.raw.tree ) do
+    if v_tree.group ~= TREE_SCALE_GROUP then
+        for i_stage = 1, TREE_GROWTH_STAGES do
+            CreateSmallTree( v_tree, i_stage )
         end
     end
 end
 
 --------------------------------------------------------------------------------
+
+--______________________________________________________________________________________________________________________
+--######################################################################################################################
